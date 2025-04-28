dad65f72cd80bbb86c2096e2e40d4e9f6d9a48e9
1 Con Mèo <0x6868@proton.me>
2025-04-28 23:21:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,11 @@ if [ ! -d "$HOME/tao-kieu-chu/fonts" ]; then
     echo "Warning: fonts directory does not exist"
     exit 1
 fi
+
+if [ ! -d "$HOME/tao-kieu-chu/views" ]; then
+    echo "Warning: views directory does not exist"
+    exit 1
+fi
 podman pull ghcr.io/43vn/tao-kieu-chu:latest
 if [ $? -ne 0 ]; then
     echo "Error: Failed to pull image"
@@ -26,6 +31,7 @@ podman run -d --name tao-kieu-chu \
     -e AES_KEY="$AES_KEY" \
     -v "$HOME/tao-kieu-chu/assets:/app/assets" \
     -v "$HOME/tao-kieu-chu/fonts:/app/fonts" \
+    -v "$HOME/tao-kieu-chu/views:/app/views" \
     --restart=always \
     --network=host \
     ghcr.io/43vn/tao-kieu-chu:latest
